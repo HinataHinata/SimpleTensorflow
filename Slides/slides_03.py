78a4663d147380378d060025e83efd669f6e6ef4
ZhuChao <291324510@qq.com>
2017-07-31 21:16:02 +0800

    添加 官方课程提供的文件

---
+++ Slides/slides_03.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+' simple tensorflow demo '
+
+__author__ = 'zhuchao'
+
+# Model relationship between a scalar dependent variable y and independent variables X
+
+# X: number o fincidents of fire
+# Y: number of incidents of theft
+
+
+
+
+
