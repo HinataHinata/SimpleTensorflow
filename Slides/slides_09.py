c1d8a0b7a4dc1c6538e2719f275bcd816bc27953
ZhuChao <291324510@qq.com>
2017-08-15 18:15:35 +0800

    reader -part

--- Slides/slides_09.py
+++ Slides/slides_09.py
@@ -7,6 +7,9 @@
 
 import tensorflow as tf
 import threading
+from PIL import  Image
+import numpy as np
+
 
 ## Agenda
 ## Data Readers Revisited
@@ -31,6 +34,9 @@ import threading
 ## tf.RandomShuffleQueue
 ## tf.PaddingFIFOQueue
 ## tf.PriorityQueue
+
+BATCH_SIZE = 2
+N_FEATURES = 9
 
 q = tf.FIFOQueue(3, "float")
 init = q.enqueue_many(([0., 0., 0.],))
@@ -91,13 +97,27 @@ with tf.Session() as sess:
     threads = tf.train.start_queue_runners(coord=coord)
     for _ in range(1):  # generate 1 example
         key, value = sess.run([key, value])  ## 计算得到的 key value 都是byte
-        print(type(key), type(value))
-        print(value)
-        print(key)
+        ## the value returned is just a string tensor,We need to use decoder to tansfer it to be our features.
+        ## we could set its record_defaults in case some spaces are empty
+        record_defaults = [[1.0] for _ in range(N_FEATURES)]
+        record_defaults[4] = [""]  # make the fifth feature string
+        record_defaults.append([1])
+        content = tf.decode_csv(value, record_defaults=record_defaults)  # content is a list
+
+        # we need convert content into tensor as features
+        content[4] = tf.cond(tf.equal(content[4], tf.constant("Present")), lambda: tf.constant(1.0),
+                             lambda: tf.constant(0.0))
+        features = tf.stack(content[:N_FEATURES])
+        label = content[-1]
+        features_result = sess.run(features)
+        print(features_result)
+        print(features, label)
+        # print(type(content), content)
+        # print(type(key), type(value))
+        # print(value)
+        # print(key)
     coord.request_stop()
     coord.join(threads)
-
-
 
 ## TFReacord
 ## TensorFlow's binary file format a serialized tf.train.Example protobuf object
@@ -135,3 +155,38 @@ with tf.Session() as sess:
 # for t in threads:
 #     t.start()
 # coord.join(threads)
+
+# minimum number elements in the queue after a dequeue,use to ensure
+# that the samples are sufficiently mixed
+# I think 10 times the BATCH_SIZE is sufficient
+min_after_dequeue = 10 * BATCH_SIZE
+
+# the maximum number of elements in the queue
+capacity = 20 * BATCH_SIZE
+
+# shuffle the data to generate BATCH_SIZE sample pairs
+data_batch, label_batch = tf.train.shuffle_batch([features, label], batch_size=BATCH_SIZE, capacity=capacity,
+                                                 min_after_dequeue=min_after_dequeue)
+
+## TFRecord
+## help overcome irrational fear of binary files
+## They make better use of cache.They are faster to move around.They can store data of different types.
+## Like many machine learning frameworks, TensorFlow has its own binary data format which is called TFRecord.
+
+def get_image_binary(filename):
+    image = Image.open(filename)
+    image = np.asarray(image,np.uint8)
+    shape = np.asarray(image.shape,np.int32)
+    return shape.tobytes(),image.tobytes()
+
+def write_to_tfrecord(balel, shape,binary_image,tfrecord_file):
+    '''
+    This example is to write a sample to TFRecord file If you want to write more samples,just use a loop
+    '''
+    writer = tf.python_io.TFRecordWriter(tfrecord_file)
+    example = tf.train.Example(features = tf.train.Feature(features={"label":tf.train.Feature(bytes_list=tf.train.BytesList(value=[label])),
+                                                                     "shape":tf.train.Feature(bytes_list=tf.train.BytesList(value=[shape])),
+                                                                     "image":tf.train.Feature(bytes_list=tf.train.BytesList(value=[binary_image]))
+                                                                     }))
+    writer.write(example.SerializeToString())
+    writer.close()
